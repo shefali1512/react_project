2113381f9a5896d8e110ad872f697af0903ecfa1
Shefali B <admin@kryptominds.com>
2022-01-05 18:19:45 +0530

    initial push

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './component/Greet';
+import Welcome from './component/welcome';
+
+function App() {
+  return (
+    <div className="App">
+       <Greet />
+       <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Greet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+// function Greet(){
+//     return <h1>Hello Guys!</h1>
+// }
+
+
+const Greet = () => <h1>Hello Guys!</h1>
+
+export default Greet
